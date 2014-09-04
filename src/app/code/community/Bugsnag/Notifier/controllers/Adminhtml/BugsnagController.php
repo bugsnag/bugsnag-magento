753755941a7b712dfc18bceb9f7bcda7c75560f7
Kyrylo Silin <kyrylosilin@gmail.com>
2014-09-03 19:16:39 -0700

    BugsnagController.php: remove cruft

--- src/app/code/community/Bugsnag/Notifier/controllers/Adminhtml/BugsnagController.php
+++ src/app/code/community/Bugsnag/Notifier/controllers/Adminhtml/BugsnagController.php
@@ -2,12 +2,6 @@
 
 class Bugsnag_Notifier_Adminhtml_BugsnagController extends Mage_Adminhtml_Controller_Action
 {
-    private static $NOTIFIER = array(
-        'name' => 'Bugsnag Magento (Official)',
-        'version' => '1.0.0',
-        'url' => 'https://bugsnag.com/notifiers/magento'
-    );
-
     public function checkAction()
     {
         Bugsnag_Notifier_Model_Observer::fireTestEvent($_POST["apiKey"]);
